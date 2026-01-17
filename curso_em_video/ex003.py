f13e7a71763c8f5da1862a5b717462660f3bdadf
Alessandro Belincanta <belincantaalessandro@gmail.com>
2026-01-16 21:56:32 -0300

    Novos exercicios

---
+++ curso_em_video/ex003.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um número: ')) #int é para dizer que o que for digitado aqui é um número inteiro.
+n2 = int(input('Digite outro número: ')) #int é para dizer que o que for digitado aqui é um número inteiro.
+soma = n1 + n2
+print('A soma dos números são', soma)
---
+++ curso_em_video/ex005.py
@@ -0,0 +1,2 @@
+msg = input('Digite algo: ')
+print(msg.isnumeric()) #se a responsta for um número será true, caso contrário será false
