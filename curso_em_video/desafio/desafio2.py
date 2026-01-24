7ebea44ce13b8b13665e03cfba66cf44f8cc12f7
Alessandro Belincanta <belincantaalessandro@gmail.com>
2026-01-23 21:54:25 -0300

    Corrigido desafio 2

--- curso_em_video/desafio/desafio2.py
+++ curso_em_video/desafio/desafio2.py
@@ -3,4 +3,4 @@ n = int(input('Digite um número: '))
 d = n*2
 t = n*3
 r = n**0.5
-print('O número digita foi {}, o dobro é {}, o triplo é {} e a raiz quadrada é {:.1f}'.format(n, d, t, r))
+print('O número digitado foi {}, o dobro é {}, o triplo é {} e a raiz quadrada é {:.1f}'.format(n, d, t, r))
