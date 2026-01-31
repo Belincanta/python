08c60907dd902c9e1189b5440f1729dcb68730ae
Alessandro Belincanta <belincantaalessandro@gmail.com>
2026-01-30 22:26:21 -0300

    Ajuste pyautogui

--- aula001/gabarito.py
+++ aula001/gabarito.py
@@ -15,11 +15,10 @@ pyautogui.PAUSE = 0.3
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
+pyautogui.PAUSE = 0.5
 
 
-# entrar no link 
-pyautogui.PAUSE = 0.3
-pyautogui.click(x=371, y=63)
+# entrar no link
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
 time.sleep(3)
