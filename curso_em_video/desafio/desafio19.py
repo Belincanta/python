8a6b801c385e226e9b64f1f6d70a193b0b0b3903
Alessandro Belincanta <belincantaalessandro@gmail.com>
2026-02-03 19:46:13 -0300

    novos desafios

---
+++ curso_em_video/desafio/desafio19.py
@@ -0,0 +1,10 @@
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+import random
+aluno1 = input('Primeiro aluno: ')
+aluno2 = input('Segundo aluno: ')
+aluno3 = input('Terceiro aluno: ')
+aluno4 = input('Quarto aluno: ')
+lista = [aluno1, aluno2, aluno3, aluno4]
+escolhido = random.choice(lista)
+print(f'O aluno escolhido foi {escolhido}')
+
