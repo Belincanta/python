1fafc55c0c0d96783d25301b23b36e592d82f798
Alessandro Belincanta <belincantaalessandro@gmail.com>
2026-02-05 20:22:51 -0300

    Novas atribuições para string

---
+++ curso_em_video/ex/ex012.py
@@ -0,0 +1,13 @@
+frase = 'Curso em Vídeo Python'
+print(frase.find('y'))
+
+
+# count é para contar quantos 'texto' tem
+# replace é para trocar o texto por outro
+# upper é para deixar maiúsculo
+# lower é para deixar minúsculo
+# len é para trazer o comprimento total
+# split é para separar o texto, cada palavra terá uma posição
+# join é para juntar os textos com algum caracter ou espaço e branco
+# find é para localizar, trazendo a sua posição
+# in é para dizer se tem ou não
