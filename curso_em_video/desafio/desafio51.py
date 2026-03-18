47dfdd2cfa2dfffcea4acd28050126de361a6c8d
Alessandro Belincanta <belincantaalessandro@gmail.com>
2026-03-18 18:44:54 -0300

    Desafio 51

---
+++ curso_em_video/desafio/desafio51.py
@@ -0,0 +1,8 @@
+#Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+
+termo = int(input("Primeiro termo: "))
+razao = int(input("Razão: "))
+decimo = termo + (10-1) * razao #cálculo para trazer o último valor de PA
+for c in range(termo, decimo + razao, razao):
+    print(c, end = " > ")
+print("ACABOU")
