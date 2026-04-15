e436ac2af9c4d6dde9bbb31eb10484803a204edc
Alessandro Belincanta <belincantaalessandro@gmail.com>
2026-04-15 11:45:01 -0300

    uso de tuplas

---
+++ curso_em_video/ex/ex.025.py
@@ -0,0 +1,16 @@
+# tuplas
+
+lanche = ("Hamburguer", "Suco", "Pizza", "Pudim")
+# quando eu quero percorrer cada item dentro da tupla
+# for comida in lanche:
+#     print(f"Eu comi {comida}")
+
+# quando eu quero percorrer cada item dentro da tupla e informar o indice do item.
+# for c in range(0, len(lanche)):
+#     print(f"Eu comi {lanche[c]} na posição {c}")
+
+# quando eu quero percorrer cada item dentro da tupla e informar o indice do item, utilizando duas variáveis na estrutura de repetição.
+# for pos, c in enumerate(lanche):
+#     print(f"Eu comi {c} na posição {pos}")
+
+print("Comi demais")
