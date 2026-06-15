3ee253baaaf2e31088a36a4f6f33da442dd9e75b
Alessandro Belincanta <belincantaalessandro@gmail.com>
2026-06-15 20:52:15 -0300

    desafio 105

---
+++ curso_em_video/desafio/desafio105.py
@@ -0,0 +1,32 @@
+# Faça um programa que tenha uma função notas() que pode receber várias notas de alunos e vai retornar um dicionário com as seguintes informações:
+# Quantidade de notas
+# A maior nota 
+# A menor nota      
+# A média da turma 
+# A situação (opcional)
+
+def notas(*n, sit=False):
+    """
+    --> função para analisar notas e situações de vários alunos.
+    :param n: uma ou mais notas dos alunos (aceita várias).
+    :param sit: valor opcional, indicando se deve ou não adicionar a situação.
+    :return: dicionário com várias informações sobre a situação da turma
+    """
+    r = dict()
+    r["Total"] = len(n)
+    r["Maior"] = max(n)  
+    r["Menor"] = min(n)
+    r["Média"] = sum(n)/len(n)
+    if sit:
+        if r["Média"] >= 7:
+            r["Situação"] = "BOA"
+        elif r["Média"] >= 5:
+            r["Situação"] = "REGULAR"
+        else:
+            r["Situação"] = "IRREGULAR"
+    return r
+
+
+# programa principal
+resp = notas(8.5, 2, 7.5, sit=True)
+print(resp)
