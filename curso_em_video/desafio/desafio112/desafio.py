25d1edf612789b0426fabbd43bc0b4c82949010a
Alessandro Belincanta <belincantaalessandro@gmail.com>
2026-07-01 21:13:17 -0300

    desafio 112

---
+++ curso_em_video/desafio/desafio112/desafio.py
@@ -0,0 +1,7 @@
+# Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado. Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função imputa(), mas com uma validação de dados para aceitar apenas valores que seja monetários.
+
+from uteis import moeda
+from uteis import dado
+
+preco = dado.leiaDinheiro("Digite o valor R$ ")
+moeda.resumo(preco)
---
+++ curso_em_video/desafio/desafio112/uteis/dado.py
@@ -0,0 +1,11 @@
+#Dentro do pacote utilidadesCeV que criamos no desafio 111, temos um módulo chamado dado. Crie uma função chamada leiaDinheiro() que seja capaz de funcionar como a função imputa(), mas com uma validação de dados para aceitar apenas valores que seja monetários.
+
+def leiaDinheiro(msg):
+    valido = False
+    while not valido:
+        entrada = input(msg).replace(',','.').strip()
+        if entrada.isalpha() or entrada == '':
+            print(f"\033[0;31mERRO! '{entrada}' é um valor inválido, tente de novo!\033[m")
+        else:
+            valido = True
+            return float(entrada)
