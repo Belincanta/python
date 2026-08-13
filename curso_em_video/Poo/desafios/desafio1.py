689a023abc716ed55b7e2749e9fbaa2c36c64111
Alessandro Belincanta <belincantaalessandro@gmail.com>
2026-08-12 21:36:15 -0300

    Desafio 1 corrigido

--- curso_em_video/Poo/desafios/desafio1.py
+++ curso_em_video/Poo/desafios/desafio1.py
@@ -1,17 +1,26 @@
 from rich import print
+from rich import inspect
 
 class Funcionario:
     """
         Permite que o funcionário se apresente
     """
+    #atributo de classe
+    empresa = "Curso em Vídeo"
+
     def __init__(self, nome, setor, cargo):
+        # atributos de instância
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
-        print(f"Olá, eu sou [green]{self.nome}[/] trabalho no setor {self.setor} no cargo de {self.cargo}, na empresa Curso em Video!")
+
+    def apresentação(self):
+        return f"Olá, eu sou [green]{self.nome}[/] trabalho no setor {self.setor} no cargo de {self.cargo}, na empresa {Funcionario.empresa}!"
 
 #declaração do objeto
 fun1 = Funcionario(nome="Alessandro", setor="Processos", cargo="Assessor")
+#inspect(fun1)
+print(fun1.apresentação())
 
 
 
